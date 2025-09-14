143b885ab31e474a38e217657f0da8fa7a5031cf
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-09-14 15:13:49 -0300

    arreglando cosas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import './styles/base.css'
+import './styles/base.css'; 
 //import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -19,7 +19,7 @@ export default function Home() {
       <h2 className="alert alert-primary mt-4">Movies now playing</h2>
        <Link to="/now-playing" className="btn btn-primary">Ver más</Link>
       <section className="row cards" id="now-playing">
-     <AMoviesFetch limit={6} />
+     <AMoviesFetch limit={4} />
       </section>
     </div>
   );
