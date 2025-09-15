6a9ffd6dcc18abb85ae87a5fa955f0157d71ff6f
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-09-15 01:12:31 -0300

    css terminado del home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -6,11 +6,11 @@ import { Link } from "react-router-dom";
 export default function Home() {
   return (
     <div className="container">
-      <h2 className="alert alert-primary">Popular movies this week</h2>
+      <h2 className="section-title">Popular movies this week</h2>
       <PMoviesFetch />
       <Link to="/popular" className="btn btn-primary mt-2">Ver más</Link>
 
-      <h2 className="alert alert-primary mt-4">Movies now playing</h2>
+     <h2 className="section-title">Movies Now Playing</h2>
       <AMoviesFetch />
       <Link to="/now-playing" className="btn btn-primary mt-2">Ver más</Link>
     </div>
