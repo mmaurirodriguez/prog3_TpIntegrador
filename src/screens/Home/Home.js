aa979528a812f45facef4ac62f104bda22568940
mmaurirodriguez <mmaurirodriguez@udesa.edu.ar>
2025-09-15 18:28:53 -0300

    modifique el PMoviesFetch

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,6 +4,9 @@ import AMoviesFetch from "../../components/AMoviesFetch/AMoviesFetch";
 import { Link } from "react-router-dom";
 
 export default function Home() {
+
+
+
   return (
     <div className="container">
       <h2 className="section-title">Popular movies this week</h2>
@@ -11,7 +14,7 @@ export default function Home() {
       <Link to="/popular" className="btn btn-primary mt-2">Ver más</Link>
 
      <h2 className="section-title">Movies Now Playing</h2>
-      <AMoviesFetch />
+      <AMoviesFetch /> 
       <Link to="/now-playing" className="btn btn-primary mt-2">Ver más</Link>
     </div>
   );
