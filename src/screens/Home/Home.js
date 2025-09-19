c56bc8e7bffb3abcd1219818dc0107a990b9bc70
AgusJara <ajaralambides@udesa.edu.ar>
2025-09-19 14:25:49 -0300

    cambios

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -4,9 +4,11 @@ import AMoviesFetch from "../../components/AMoviesFetch/AMoviesFetch";
 import PopularSeriesFetch from "../../components/PopularSeriesFetch/PopularSeriesFetch";
 import TopRatedSeriesFetch from "../../components/TopRatedSeriesFetch/TopRatedSeriesFetch";
 import { Link } from "react-router-dom";
+import BusquedaFiltrada from "../../components/BusquedaFiltrada/BusquedaFiltrada";
 
 export default function Home() {
   return (
+    <React.Fragment>
     <div className="container">
       <h2 className="section-title">Popular movies this week</h2>
       <PMoviesFetch />
@@ -24,5 +26,6 @@ export default function Home() {
       <TopRatedSeriesFetch />
       <Link to="/TopRatedSeries" className="btn btn-primary mt-2">Ver más</Link>
     </div>
+    </React.Fragment>
   );
 }
