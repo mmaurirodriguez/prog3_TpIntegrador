602a47acaa45103e49e9ac67609a6d54475fcd94
clemendechazal <cdechazal@udesa.edu.ar>
2025-09-28 22:55:34 -0300

    correcion que vimos en el meet

--- src/components/LinkHeader/LinkHeader.js
+++ src/components/LinkHeader/LinkHeader.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
-const LinkHeader = (props) => {
+function LinkHeader (props) {
   return (
     <li>
-        <Link to ={props.to} >{props.text}</Link>
+      <Link to={props.to}>{props.text}</Link> 
     </li>
-  )
+  );
 }
 
-export default LinkHeader
+export default LinkHeader ;
